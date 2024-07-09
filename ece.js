019293f523fea9075d851d40d541365112862d4f
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-09 19:36:57 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,8 @@
+const baslik=document.getElementById("ece");
+console.log(baslik);
+
+const hatalar=document.getElementsByClassName("error");
+console.log(hatalar[0]);
+
+const petiketleri=document.getElementsByTagName("p");
+console.log(petiketleri[1]);
